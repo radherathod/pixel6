2cae719b2d87179ee5de3e03e0f301efa3ac9892
Radhe <65850061+radherathod@users.noreply.github.com>
2024-08-05 14:33:51 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-// // src/index.js
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { QueryClient, QueryClientProvider } from "react-query";
-// import App from "./App";
-// import "./index.css";
-
-// const queryClient = new QueryClient();
-
-// ReactDOM.render(
-//   <QueryClientProvider client={queryClient}>
-//     <App />
-//   </QueryClientProvider>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client"; // Import the new API
 import App from "./App";
